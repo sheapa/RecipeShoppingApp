9d946c78f7fa9f98a1c8e2d9fc21efd7718a5bc6
sheapa <patshea26@gmail.com>
2022-07-14 15:42:29 -0400

    components made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/UI/header/header.component';
+import { ShoppingListEditComponent } from './Components/Shopping/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './Components/Shopping/shopping-list/shopping-list.component';
+import { RecipeComponent } from './Components/Recipe/recipe/recipe.component';
+import { RecipeListComponent } from './Components/Recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Components/Recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Components/Recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListEditComponent,
+    ShoppingListComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
