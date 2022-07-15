552e285f5d4c5b730a88b460365cdca3cf7e91a4
sheapa <patshea26@gmail.com>
2022-07-14 20:57:46 -0400

    recipe list

--- src/app/Components/Recipe/recipe-list/recipe-list.component.ts
+++ src/app/Components/Recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Grilled Cheese', 'Savory warm cheese sandwich', 'https://live.staticflickr.com/4017/4541489084_968537a08f_b.jpg')
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
--- src/app/Components/Recipe/recipe/recipe.component.ts
+++ src/app/Components/Recipe/recipe/recipe.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent implements OnInit {
+ 
 
   constructor() { }
 
