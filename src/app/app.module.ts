deffac595adfdba710227cf0c2eb1d5c12e73256
sheapa <patshea26@gmail.com>
2022-07-16 14:19:05 -0400

    overhaul

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Components/UI/header/header.component';
-import { ShoppingListEditComponent } from './Components/Shopping/shopping-list-edit/shopping-list-edit.component';
-import { ShoppingListComponent } from './Components/Shopping/shopping-list/shopping-list.component';
-import { RecipeComponent } from './Components/Recipe/recipe/recipe.component';
-import { RecipeListComponent } from './Components/Recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './Components/Recipe/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './Components/Recipe/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListEditComponent,
-    ShoppingListComponent,
-    RecipeComponent,
+    RecipesComponent,
     RecipeListComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
